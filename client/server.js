bb0fa23280ca101d64cae71a1d9169b15658df0f
arizarshal <94399272+arizarshal@users.noreply.github.com>
2022-02-17 14:33:23 +0530

    Update server.js

--- client/server.js
+++ client/server.js
@@ -9,80 +9,80 @@ app.get('/', function (req, res) {
 });
 
 app.get('/wallet', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/nft-dev', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/defi', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 
 app.get('/coin', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/blockchain', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/staking', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/hybrid', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/dex', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/marginex', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/p2p', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/sec_ex', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/sec_token', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/asset_token', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/stable', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/ico', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/nftGames', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/blog', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/api/blog/product/:productId', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.get('/about', function (req, res) {
-  res.sendFile(path.join(__dirname, '../innovacio/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 
